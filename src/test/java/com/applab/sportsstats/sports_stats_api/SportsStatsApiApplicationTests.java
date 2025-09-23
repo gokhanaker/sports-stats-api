43160f6e885e4b477a3c10f7e9b437ea8b3049e0
gokhanaker <gokhanaker@gmail.com>
2025-09-23 12:02:20 +0300

    feat: initialized sports-stats-api project

---
+++ src/test/java/com/applab/sportsstats/sports_stats_api/SportsStatsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applab.sportsstats.sports_stats_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsStatsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
